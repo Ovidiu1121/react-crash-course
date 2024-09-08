23e60b71bbfdf3a7c3467d4f3e2a840bc6922c05
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-08 14:28:49 +0300

    added useState

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,22 @@
 import styles from './ProductCard.module.css'
+import { useState } from 'react';
 
 
 export function ProductCard({ product, background = "slategray", onPurchase }) {
+
+    let [stockCount, setStockCount] = useState(product.stockCount);
+    let [showMore, setShowMore] = useState(false);
+
+    function handleClick() {
+        setStockCount((previousStockCount) => previousStockCount - 1);
+        onPurchase(product)
+    }
+
+    function handleTwoClicks() {
+
+        setStockCount((previousStockCount) => previousStockCount - 1);
+        setStockCount((previousStockCount) => previousStockCount - 1);
+    }
 
     return (
         <article className={styles.Container} style={{ background }}>
@@ -12,15 +27,23 @@ export function ProductCard({ product, background = "slategray", onPurchase }) {
                 width={128}
                 height={128}
             />
-            <p>Specification:</p>
-            <ul className={styles.Specification}>
+            <p>Specification:
+                <button onClick={() => setShowMore(!showMore)}>{showMore ? 'hide' : 'show'}</button>
+            </p>
+            {showMore && (<ul className={styles.Specification}>
                 {product.specification.map((spec, index) => (
                     <li key={index}>{spec}</li>
                 ))}
-            </ul>
-            <Status stockCount={product.stockCount} />
-            {product.stockCount > 0 && (
-                <button onClick={() => onPurchase(product)}>Buy (From ${product.price})</button>
+            </ul>)}
+            <Status stockCount={stockCount} />
+            {stockCount > 0 && (
+                <>
+                    <p>Price: ${product.price}</p>
+                    <button onClick={handleClick}>Buy</button>
+                </>
+            )}
+            {stockCount > 1 && (
+                <button onClick={handleTwoClicks}>Buy 2</button>
             )}
         </article>
     );
